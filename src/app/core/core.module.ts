7578579462b42a14a70a3360e1aa3c4b3e84d7f6
Kliment <kliment1986@inbox.ru>
2017-11-21 07:30:44 +0300

    Небольшая переработка структуры.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,15 @@
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ContentContainerComponent } from './components/content-container/content-container.component';
 
 /**
  * https://angular.io/guide/styleguide#core-feature-module
  */
 @NgModule({
   imports: [CommonModule, RouterModule],
-  declarations: [HeaderComponent],
-  exports: [HeaderComponent]
+  declarations: [HeaderComponent, ContentContainerComponent],
+  exports: [HeaderComponent, ContentContainerComponent]
 })
 export class CoreModule {}
