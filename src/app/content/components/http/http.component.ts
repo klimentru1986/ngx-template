239947a9e93afa849b604c7680248ada7dc4e828
klimentru1986 <kliment1986@inbox.ru>
2018-03-10 23:02:44 +0300

    Работа со списком через async

--- src/app/content/components/http/http.component.ts
+++ src/app/content/components/http/http.component.ts
@@ -1,10 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
 import { HttpComponentInteropService } from './../../services/http-component-interop/http-component-interop.service';
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs/Subscription';
 import { ResponseModel } from '../../models/response.model';
 import { MockUser } from '../../models/mock-user.model';
 
 import 'rxjs/add/operator/first';
+import { Observable } from 'rxjs/Observable';
 
 /** Компонент с примером http запроса */
 @Component({
@@ -12,12 +13,10 @@ import 'rxjs/add/operator/first';
   templateUrl: './http.component.html',
   styleUrls: ['./http.component.scss']
 })
-export class HttpComponent implements OnInit, OnDestroy {
+export class HttpComponent implements OnInit {
   constructor(private httpInterop: HttpComponentInteropService) {}
   /** респонс для observable запроса */
-  public responseFromObservable: ResponseModel;
-  /** Подписка на получение данных */
-  private dataSubscription: Subscription;
+  public responseFromObservable$: Observable<ResponseModel>;
   /** получение мокового списка пользователей */
   public mockUsers: MockUser[];
   /** Сообщение об ошибке */
@@ -25,20 +24,13 @@ export class HttpComponent implements OnInit, OnDestroy {
 
   /** Событие жизненного цикла */
   ngOnInit() {
-    this.getDataFromObservable();
+    this.responseFromObservable$ = this.getDataFromObservable();
     this.getMockUsers();
   }
 
-  /** Событие жизненного цикла */
-  ngOnDestroy() {
-    this.dataSubscription.unsubscribe();
-  }
-
   /** Получение данных из observable запроса */
-  getDataFromObservable(): void {
-    this.dataSubscription = this.httpInterop
-      .getAsObservable()
-      .subscribe(response => (this.responseFromObservable = response));
+  getDataFromObservable(): Observable<ResponseModel> {
+    return this.httpInterop.getAsObservable();
   }
 
   /** Получение данных с мокового сервера */
